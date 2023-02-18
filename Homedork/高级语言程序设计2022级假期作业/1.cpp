2bef82edaa2e943bd4d733d433c5053f8928e654
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2023-02-18 19:56:06 +0800

    Create 1.cpp

---
+++ Homedork/高级语言程序设计2022级假期作业/1.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <math.h>
+
+#include <string.h>
+
+int ba(int *p,int n,int x)
+
+{
+
+    int j;
+
+    for(j=0;j<n;j++)
+
+            if(p[j]>=x)
+
+                printf("%d ",p[j]);
+
+    return 0;
+
+}
+
+int main()
+
+{
+
+    int i,n,x,a[100];
+
+    while(scanf("%d %d",&n,&x)!=EOF)
+
+    {
+
+        for(i=0;i<n;i++)
+
+            scanf("%d",&a[i]);
+
+        ba(a,n,x);
+
+        printf("\n");
+
+    }
+
+    return 0;
+
+}
+
