9cfc7b4f6f687a852136b1d15b22fd82a31318a9
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2023-02-28 18:37:44 +0800

    动态二维数组创建
    double (*a)[5]=NULL,(*p)[5]=NULL；
    a = (double (*)[5]) malloc(n*5*sizeof(double));
           if(a==NULL) exit(1);

--- Homedork/高级语言程序设计2022级假期作业/7.cpp
+++ Homedork/高级语言程序设计2022级假期作业/7.cpp
@@ -1,43 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+
 int main()
 {
-    int i,n,j;
-    double *m,*sum;
-    //double m[10],sum[10];
-    double **a;
-    while(scanf("%d",&n)!=EOF)
-    {
-        a=(double **)malloc(sizeof(double *)*3);
-        m=(double *)malloc(sizeof(double)*n);
-        sum=(double *)malloc(sizeof(double)*n);
-        for(i=0; i<3; i++)
-            a[i]=(double *)malloc(sizeof(double));
-        for(i=0; i<n; i++)
-            sum[i]=m[i]=0;
-        for(i=0; i<n; i++)
-        {
-            for(j=0; j<3; j++)
-            {
-                scanf("%lf",&a[i][j]);
-                sum[i]+=a[i][j];
-            }
-            m[i]=sum[i]/3;
-        }
-        for(i=0; i<n; i++)
-        {
-            for(j=0; j<3; j++)
-            {
-                printf("%.2lf ",a[i][j]);
-            }
-            printf("%.2lf %.2lf\n",sum[i],m[i]);
-        }
-    }
-    free(a[i]);
-    free(a);
-    free(sum);
-    free(m);
+   int n,i,j;
+   double (*a)[5]=NULL,(*p)[5]=NULL,*q,sum;
+   while(scanf("%d",&n)!=EOF)
+   {
+       a = (double (*)[5]) malloc(n*5*sizeof(double));
+       if(a==NULL) exit(1);
+       for(p=a;p<a+n;p++)
+       {
+           sum = 0;
+           for(q=*p;q<*p+3;q++)
+           {
+               scanf("%lf",q);
+               sum += *q;
+           }
+           *q++ = sum;
+           *q = sum/3;
+       }
+
+       for(p=a;p<a+n;p++)
+       {
+           for(q=*p;q<*p+5;q++)  printf("%.2f ",*q);
+           printf("\n");
+       }
+
+   }
+
     return 0;
+
 }
