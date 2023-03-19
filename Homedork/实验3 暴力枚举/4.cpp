e4eb284c65c33025817125a9fdc59c0a58490d33
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2023-03-19 14:58:08 +0800

    二进制枚举

---
+++ Homedork/实验3 暴力枚举/4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+​using namespace std;
+int main()
+{
+    int i,j,n,T,x,aso;
+    cin>>T;
+    n=0;
+    for(i=1;i<1<<14;i++)
+    {
+        x=T;
+        aso=0;
+        for(j=0;j<14;j++)
+        {
+            if(i&1<<j)
+                x*=2,aso++;
+            else x--;
+        }
+        if(x==1&&aso==5)
+            n++;
+    }
+    cout<<n<<endl;
+    return 0;
+}
