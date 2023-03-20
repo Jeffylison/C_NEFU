cd1b5e7ac18438539d8e6a99c56d6cec643f404d
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2023-03-20 19:30:06 +0800

    next_permutation使用必须数组升序

---
+++ 一些常用代码/正序全排列.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    do
+    {
+        cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" "<<a[4]<<endl;
+    }while(next_permutation(a,a+5));
+    return 0;
+}
