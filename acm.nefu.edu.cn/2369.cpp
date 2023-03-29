3f188b2e5704b151257ef2c2ba6da098e5aa74f3
Jeffylison <100355251+Jeffylison@users.noreply.github.com>
2023-03-29 16:38:01 +0800

    Create 2369.cpp

---
+++ acm.nefu.edu.cn/2369.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+int isprime(int n)
+{
+    if(n<2) return 0;
+    int i;
+    for(i=2;i<n;i++)
+        if(n%i==0) return 0;
+    return 1;
+}
+int main()
+{
+    int n,i,sum;
+    while(cin>>n)
+    {
+        sum=0;
+        for(i=2;i<n/2;i++)
+        {
+            if(isprime(i)&&isprime(n-i))
+                sum++;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
